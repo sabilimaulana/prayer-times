3cdcd8cf4056a152189ef45ab2fd6de02bba8357
sabilimaulana <spacysbl.learn@gmail.com>
2021-09-01 16:57:58 +0700

    Get geolocation

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
+const success = (position) => {
+  console.log(position);
+};
+
+const error = () => {
+  alert("Posisi tidak dapat diakses");
+};
+
+const userLocation = () => {
+  if (!navigator.geolocation) {
+    alert(
+      "Geolocation tidak didukung di dalam browser kamu, silahkan gunakan browser lain"
+    );
+  } else {
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+};
+
 const index = () => {
   let app = document.getElementById("app");
   let h3 = document.createElement("h3");
   h3.innerHTML = "Prayer Times";
 
   app.appendChild(h3);
+
+  userLocation();
 };
 
 index();
